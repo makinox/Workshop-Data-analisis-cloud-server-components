7d8e579a40e45700302bd64988c3e176c0054dcc
Jesus Bossa <x@jesusbossa.dev>
2023-07-12 20:55:10 -0500

    feat: render table of data

--- apps/fife-front/app/page.tsx
+++ apps/fife-front/app/page.tsx
@@ -3,13 +3,34 @@ import { readDocument } from '@/utils';
 import styles from './page.module.css';
 
 const Home = async () => {
-  const teamsData = await readDocument();
+  const { tableHeaders, teamsData } = await readDocument();
+
   console.log({ teamsData });
 
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h2>Hola mundo</h2>
+        <table>
+          <thead>
+            <tr>
+              {tableHeaders.map((header) => (
+                <th>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {teamsData.map((data) => (
+              <tr>
+                <td>{data.equipo}</td>
+                <td>{data.partido1}</td>
+                <td>{data.partido2}</td>
+                <td>{data.partido3}</td>
+                <td>{data.partido4}</td>
+                <td>{data.partido5}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   );
--- apps/fife-front/utils/index.ts
+++ apps/fife-front/utils/index.ts
@@ -41,5 +41,5 @@ export const readDocument = async () => {
     return rowData as teamData;
   });
 
-  return teamsData;
+  return { teamsData, tableHeaders };
 };
