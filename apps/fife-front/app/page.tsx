897a3fc6aeda5daf7440482e614c1ea1f2bd82ac
Jesus Bossa <x@jesusbossa.dev>
2023-07-13 11:42:36 -0500

    feat: render table graphics

--- apps/fife-front/app/page.tsx
+++ apps/fife-front/app/page.tsx
@@ -1,39 +1,14 @@
+import { AppBar } from '@/components/AppBar';
 import { readDocument } from '@/utils';
 
 import styles from './page.module.css';
-import { AppBar } from '@/components/AppBar';
 
 const Home = async () => {
   const { tableHeaders, teamsData } = await readDocument();
 
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <table>
-          <thead>
-            <tr>
-              {tableHeaders.map((header) => (
-                <th>{header}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {teamsData.map((data) => (
-              <tr>
-                <td>{data.equipo}</td>
-                <td>{data.partido1}</td>
-                <td>{data.partido2}</td>
-                <td>{data.partido3}</td>
-                <td>{data.partido4}</td>
-                <td>{data.partido5}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <section style={{ width: '300px', height: '300px' }}>
-        <AppBar tableHeaders={tableHeaders} teamsData={teamsData} />
-      </section>
+      <AppBar tableHeaders={tableHeaders} teamsData={teamsData} />
     </main>
   );
 };
--- apps/fife-front/components/AppBar.tsx
+++ apps/fife-front/components/AppBar.tsx
@@ -1,8 +1,9 @@
 'use client';
-
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 import { teamData } from '@/utils';
+
+const formatTeamName = (teamName: string) => `Equipo ${teamName}`;
 
 interface IAppBar {
   tableHeaders: Array<string>;
@@ -10,34 +11,44 @@ interface IAppBar {
 }
 
 export const AppBar = ({ tableHeaders, teamsData }: IAppBar) => {
-  const tableData = tableHeaders.slice(1).map((header, index) => {
-    return {
-      name: header,
-      nombre: teamsData[0].partido1,
-    };
+  const teamParseValues = teamsData.map((team) => {
+    const teamName = team.equipo;
+    const values = Object.values(team).slice(1);
+
+    return { teamName, values };
   });
 
+  const tableData = tableHeaders.slice(1).map((header, index) => ({
+    name: header,
+    [formatTeamName(teamParseValues[0].teamName)]: teamParseValues[0].values[index],
+    [formatTeamName(teamParseValues[1].teamName)]: teamParseValues[1].values[index],
+    [formatTeamName(teamParseValues[2].teamName)]: teamParseValues[2].values[index],
+  }));
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={tableData}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
-    </ResponsiveContainer>
+    <section style={{ width: '800px', height: '800px' }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={800}
+          height={500}
+          data={tableData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey={formatTeamName(teamParseValues[0].teamName)} fill="#8884d8" />
+          <Bar dataKey={formatTeamName(teamParseValues[1].teamName)} fill="#82ca9d" />
+          <Bar dataKey={formatTeamName(teamParseValues[2].teamName)} fill="#db4747" />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
   );
 };
